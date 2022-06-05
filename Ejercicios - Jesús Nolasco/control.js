7d1a72bb873f4957ded9c03f414adccd6171376f
Jesus-NG <jesus.ng119@gmail.com>
2022-06-05 02:33:04 -0500

    Se añadió proyecto

---
+++ Ejercicios - Jesús Nolasco/control.js
@@ -0,0 +1,28 @@
+// ¿Qué quieres videojuego quieres jugar? 1=Halo, 2=God of War, 3=Gears of War, 4=Star Wars
+
+let respuesta1 = 'Halo'
+let respuesta2 = 'God of War'
+let respuesta3 = 'Gears of War'
+let respuesta4 = 'Star Wars'
+
+let respuesta = respuesta4
+
+if(respuesta == respuesta1)
+{
+    console.log('Bienvenido Jefe Maestro.')
+}
+
+if(respuesta == respuesta2)
+{
+    console.log('Los dioses del olimpo me han abandonado.')
+}
+
+if(respuesta == respuesta3)
+{
+    console.log('Que detalle.')
+}
+
+if(respuesta == respuesta4)
+{
+    console.log('Luke, yo soy tu padre.')
+}
---
+++ Ejercicios - Jesús Nolasco/index.js
@@ -0,0 +1,60 @@
+let simbolo1 = ' _________________________________'
+console.log(simbolo1)
+let simbolo2 = '│:::::::::::::;;::::::::::::::::::│'
+console.log(simbolo2)
+let simbolo3 = '│:::::::::::`~││~~~``:::::::::::::│'
+console.log(simbolo3)
+let simbolo4 = '│::::::::`   .`:     o`:::::::::::│'
+console.log(simbolo4)
+let simbolo5 = '│:::::::` oo │ │o  o    ::::::::::│'
+console.log(simbolo5)
+let simbolo6 = '│::::::: 8  .`.`    8 o  :::::::::│'
+console.log(simbolo6)
+let simbolo7 = '│::::::: 8  │ │     8    :::::::::│'
+console.log(simbolo7)
+let simbolo8 = '│::::::: _._│ │_,...8    :::::::::│'
+console.log(simbolo8)
+let simbolo9 = '│::::::`~--.   .--. `.   `::::::::│'
+console.log(simbolo9)
+let simbolo10 = '│:::::`     ═8     ~  ` o ::::::::│'
+console.log(simbolo10)
+let simbolo11 = '│::::`       8._ 88.   ` o::::::::│'
+console.log(simbolo11)
+let simbolo12 = '│:::`   __. ,.ooo~~.    ` o`::::::│'
+console.log(simbolo12)
+let simbolo13 = '│:::   . -. 88`78o/:     `  `:::::│'
+console.log(simbolo13)
+let simbolo14 = '│::`     /. o o \ ::       `88`::::│'
+console.log(simbolo14)
+let simbolo15 = '│:;     o|| 8 8 |d.        `8 `:::│'
+console.log(simbolo15)
+let simbolo16 = '│:.       — ^ ^ —`           `—`::│'
+console.log(simbolo16)
+let simbolo17 = '│::.                          .:::│'
+console.log(simbolo17)
+let simbolo18 = '│:::::.....           ::`     ``::│'
+console.log(simbolo18)
+let simbolo19 = '│::::::::-``-        88          `│'
+console.log(simbolo19)
+let simbolo20 = '│:::::-`.          -       ::     │'
+console.log(simbolo20)
+let simbolo21 = '│:-~. . .                   :     │'
+console.log(simbolo21)
+let simbolo22 = '│ .. .   ..:   o:8      88o       │'
+console.log(simbolo22)
+let simbolo23 = '│. .     :::   8:P     d888. . .  │'
+console.log(simbolo23)
+let simbolo24 = '│.   .   :88   88      888`  . .  │'
+console.log(simbolo24)
+let simbolo25 = '│   o8  d88P . 88   ` d88P   ..   │'
+console.log(simbolo25)
+let simbolo26 = '│  88P  888   d8P   ` 888         │'
+console.log(simbolo26)
+let simbolo27 = '│   8  d88P.`d:8  .- dP~ o8       │'
+console.log(simbolo27)
+let simbolo28 = '│      888   888    d~ o888    JN │'
+console.log(simbolo28)
+let simbolo29 = '│_________________________________│'
+console.log(simbolo29)
+let simbolo30 = '           DARTH VADER             '
+console.log(simbolo30)
