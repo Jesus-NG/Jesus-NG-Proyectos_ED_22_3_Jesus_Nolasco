08ffe6a52633d40f875b310f41c3afe1e3c75e5e
UNITEC <Unitec.labcom@gmail.com>
2022-06-06 12:05:18 -0500

    se agregaron objetos

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Jesús Horacio Nolasco Gallegos
+//Laura Gonzalez Sandoval
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+        public GameObject bolita;
+        public GameObject cuadrado;
+        public GameObject hexagono;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
