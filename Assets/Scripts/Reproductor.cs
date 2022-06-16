b5a396ceb3f6329b36dbda07b7c7b26bdbdb7b3a
Jesus-NG <jesus.ng119@gmail.com>
2022-06-16 00:54:05 -0500

    Se agrego audio

--- Assets/Scripts/Reproductor.cs
+++ Assets/Scripts/Reproductor.cs
@@ -5,43 +5,56 @@ using UnityEngine;
 public class Reproductor : MonoBehaviour
 {
 [SerializeField]
-    AudioSource audioS;
-    public AudioClip[] ListaAudio;
-    public int indice = 0;
+    AudioSource audioS; //Hace una referencia al componente que quiero manipular.
+    public AudioClip[] ListaAudio; //Crea un arreglo de clipsde audio.
+    public int indice = 0; //Un valor mutable para recorrer el arreglo.
 
     // Start is called before the first frame update
     void Start()
     {
-        audioS = GetComponent<AudioSource>();
+        audioS = GetComponent<AudioSource>(); //Enlaza o lee el componente AudioSource en el objeto.
+        //Propietario de este script.
         //ListaAudio = new AudioClip[3];
 
-        audioS.clip = ListaAudio[indice];
-
+        audioS.clip = ListaAudio[indice]; //Asigna desde la lista el valor inicial que debe reproucir el audiosource.
+        audioS.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)) //Asigno una tecla.
         {
-              indice++; 
-              if (indice >= ListaAudio.Length)
+          AumentarIndice();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B)) //Asigno una tecla.
+        {
+          DisminuirIndice();
+        }
+
+        audioS.clip = ListaAudio[indice]; //Actualiza el valor del cli asignado a el AudioSource.
+        audioS.Play();
+    }//Fin update
+
+    //Aqui inician los metodos o funciones
+    public void AumentarIndice()
+    {
+      indice++; //Aumento el indice.
+              if (indice >= ListaAudio.Length) //Comparo el valor el indice con la cantidad de elementos.
               {
                 Debug.Log("Has llegado a la meta");
-                indice = ListaAudio.Length-1;
+                indice = ListaAudio.Length-1; //Asigno a indice el ultimo valor conocido de la lista.
               }
-        }
+    }
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-              indice--; 
-              if (indice <= 0)
+    public void DisminuirIndice()
+    {
+      indice--; //Disminuyo el indicce
+              if (indice <= 0) //Comparo el valor de indice con la cantidad de elementos.
               {
                 Debug.Log("Has regresado");
-                indice = 0;
+                indice = 0; //Asigno a indice el ultimo valor coocido en la lista.
               }
-        }
-
-        audioS.clip = ListaAudio[indice];
     }
-}
+}//Fin de clase
